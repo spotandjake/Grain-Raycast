071d8fb5db306e1bc489e51bb363d7207d05c307
Spotandjake <spotandjake@hotmail.com>
2024-11-11 22:23:20 -0500

    feat: Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import { ActionPanel, List, OpenInBrowserAction, showToast, ToastStyle } from "@raycast/api";
 import { useEffect, useState } from "react";
-import algoliasearch from "algoliasearch/lite";
+import { algoliasearch, type SearchResponse } from "algoliasearch";
 
+interface DocumentationItem {
+  title: string;
+  subTitle: string;
+  url: string;
+  detail: string;
+}
+type Result<T> = { isErr: true; err: string } | { isErr: false; data: T };
 export default function main() {
   // Algolia Preferences
   const algoliaConfig = {
@@ -11,49 +18,61 @@ export default function main() {
   };
   // Algolia Client
   const algoliaClient = algoliasearch(algoliaConfig.appId, algoliaConfig.apiKey);
-  const algoliaIndex = algoliaClient.initIndex(algoliaConfig.indexName);
-
   // Search
-  const [searchResults, setSearchResults] = useState<any[] | undefined>();
+  const [searchResults, setSearchResults] = useState<DocumentationItem[] | undefined>();
   const [isLoading, setIsLoading] = useState(false);
-  const search = async (query = "") => {
+  const search = async (query: string) => {
     setIsLoading(true);
-    return await algoliaIndex
-      .search(query)
-      .then((res) => {
-        setIsLoading(false);
-        return res.hits;
+    const searchResponse = await algoliaClient
+      .searchSingleIndex({
+        indexName: algoliaConfig.indexName,
+        searchParams: { query: query },
       })
-      .catch((err) => {
-        setIsLoading(false);
-        showToast(ToastStyle.Failure, "Algolia Error", err.message);
-        return [];
+      .then((r): Result<SearchResponse<unknown>> => ({ isErr: false, data: r }))
+      .catch((err): Result<SearchResponse<unknown>> => ({ isErr: true, err: `${err.message}` }));
+    setIsLoading(false);
+    if (searchResponse.isErr) {
+      showToast(ToastStyle.Failure, "Algolia Error", searchResponse.err);
+      return [];
+    }
+    // Build Query Responses
+    const resultItems = [];
+    for (const result of searchResponse.data.hits) {
+      //eslint-disable-next-line
+      const unknownResult = result as any;
+      if (unknownResult.type == "lvl2") continue;
+      const resultPath = Object.values(unknownResult.hierarchy)
+        .filter((n) => n != null)
+        .join(" > ");
+      console.log(result);
+      // Try Build Detail
+      const docRawPath = unknownResult.url.replace(
+        "https://raw.githubusercontent.com/grain-lang/grain/refs/heads/main/stdlib/stdlib/"
+      );
+      const docContent = await fetch(docRawPath);
+      // const url = result.url || "";
+      resultItems.push({
+        title: unknownResult.hierarchy[unknownResult.type],
+        subTitle: resultPath,
+        url: unknownResult.url,
+        detail: "",
       });
+    }
+    // Return
+    return resultItems;
   };
   useEffect(() => {
-    (async () => setSearchResults(await search()))();
+    (async () => setSearchResults(await search("")))();
   }, []);
   // Display List
   const searchItems = searchResults?.map((result) => {
-    console.log(result);
-    // Get Result Information
-    const resultPath = Object.values(result.hierarchy).filter((v) => v !== null);
-    const targetLevel = result.type;
-    const title = result.hierarchy[targetLevel];
-    const subtitle = [];
-    for (const pathItem of resultPath) {
-      subtitle.push(pathItem);
-    }
-    // Build List Item Detail
-    // const listDetail = (
-    //   <List.Item.Detail markdown="![Illustration](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png)" />
-    // );
     // Build List Item
     return (
       <List.Item
-        key={result.objectID}
-        title={title}
-        subtitle={subtitle.join(" > ")}
+        // TODO: What is a good key here?
+        key={result.url}
+        title={result.title}
+        subtitle={result.subTitle}
         actions={
           <ActionPanel title={result.url}>
             <OpenInBrowserAction url={result.url} title="Open in Browser" />
