0e79ebbcd898b0acfadae4ee956f5c26c93b393b
Spotandjake <spotandjake@hotmail.com>
2024-11-11 21:57:32 -0500

    feat: Initial version

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,23 @@
-import { ActionPanel, getPreferenceValues, List, OpenInBrowserAction, showToast, ToastStyle } from "@raycast/api";
-import { useEffect, useMemo, useState } from "react";
+import { ActionPanel, List, OpenInBrowserAction, showToast, ToastStyle } from "@raycast/api";
+import { useEffect, useState } from "react";
 import algoliasearch from "algoliasearch/lite";
 
 export default function main() {
-  const preferences = getPreferenceValues();
+  // Algolia Preferences
+  const algoliaConfig = {
+    appId: "CDP8WGS04L",
+    apiKey: "fb27c36015ee3fd18739deeb1be5f71b",
+    indexName: "grain-lang",
+  };
+  // Algolia Client
+  const algoliaClient = algoliasearch(algoliaConfig.appId, algoliaConfig.apiKey);
+  const algoliaIndex = algoliaClient.initIndex(algoliaConfig.indexName);
 
-  const algoliaClient = useMemo(() => {
-    return algoliasearch(preferences.appId, preferences.apiKey);
-  }, [preferences.appId, preferences.apiKey]);
-
-  const algoliaIndex = useMemo(() => {
-    return algoliaClient.initIndex(preferences.indexName);
-  }, [algoliaClient, preferences.indexName]);
-
+  // Search
   const [searchResults, setSearchResults] = useState<any[] | undefined>();
   const [isLoading, setIsLoading] = useState(false);
-
   const search = async (query = "") => {
     setIsLoading(true);
-
     return await algoliaIndex
       .search(query)
       .then((res) => {
@@ -31,31 +30,45 @@ export default function main() {
         return [];
       });
   };
-
   useEffect(() => {
     (async () => setSearchResults(await search()))();
   }, []);
-
+  // Display List
+  const searchItems = searchResults?.map((result) => {
+    console.log(result);
+    // Get Result Information
+    const resultPath = Object.values(result.hierarchy).filter((v) => v !== null);
+    const targetLevel = result.type;
+    const title = result.hierarchy[targetLevel];
+    const subtitle = [];
+    for (const pathItem of resultPath) {
+      subtitle.push(pathItem);
+    }
+    // Build List Item Detail
+    // const listDetail = (
+    //   <List.Item.Detail markdown="![Illustration](https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png)" />
+    // );
+    // Build List Item
+    return (
+      <List.Item
+        key={result.objectID}
+        title={title}
+        subtitle={subtitle.join(" > ")}
+        actions={
+          <ActionPanel title={result.url}>
+            <OpenInBrowserAction url={result.url} title="Open in Browser" />
+          </ActionPanel>
+        }
+      />
+    );
+  });
   return (
     <List
       throttle={true}
       isLoading={isLoading || searchResults === undefined}
-      onSearchTextChange={async (query) => setSearchResults(await search(query))}
+      onSearchTextChange={async (q: string) => setSearchResults(await search(q))}
     >
-      {searchResults?.map((result) => (
-        <List.Item
-          key={result.objectID}
-          title={result[preferences.mainAttribute]}
-          subtitle={preferences.secondaryAttribute ? result[preferences.secondaryAttribute] : undefined}
-          actions={
-            preferences.urlAttribute ? (
-              <ActionPanel title={result.name}>
-                <OpenInBrowserAction url={result.url} title="Open in Browser" />
-              </ActionPanel>
-            ) : undefined
-          }
-        />
-      ))}
+      {searchItems}
     </List>
   );
 }
